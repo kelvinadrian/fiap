59d2f6efa9a0ed4637c2732bf01e2b888b8b2e17
Kelvin Adrian Vieira <kelvin_adrian3@hotmail.com>
2024-05-12 18:31:17 -0300

    implementation terraform k8s

---
+++ terraform/terraform.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.49.0"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.30.0"
+    }
+  }
+
+  required_version = "~> 1.3"
+}
